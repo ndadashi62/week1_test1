167be83606a2eb67f5c5d5b94656f572a2110142
Nasim <nasim@Mostafas-MacBook-Pro.local>
2021-08-09 16:23:09 -0400

    Hello java added  to src

---
+++ src/Hello.java
@@ -0,0 +1,8 @@
+import com.sun.scenario.effect.impl.sw.sse.SSEBlend_SRC_OUTPeer;
+
+public class Hello {
+    public static void main(String[] args) {
+        System.out.println("ATM machine");
+        System.out.println("");
+    }
+}
