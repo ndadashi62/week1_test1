b52d7b2426331c444bb0f5549f2832bb599ac7f1
ndadashi62 <nasim.dadashi@gmail.com>
2021-08-10 16:08:48 -0400

    the Menuofchampion is added and updated with hard codes

---
+++ exercise/src/main/java/com/mthree/exercise2/MenuOfChampions.java
@@ -0,0 +1,43 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mthree.exercise2;
+
+/**
+ *
+ * @author mostafa
+ */
+public class MenuOfChampions {
+    
+    public static void main(String[] args) {
+
+    String soup= "Borocoly";
+    String kabab = "Kabab";
+    String taco = "Taco";
+    
+    double soupPrice = 10;
+    double kababPrice = 12;
+    double tacoPrice = 30;
+    
+    System.out.println("WELCOME TO RESTAURANT NIGHT VALE!");
+    System.out.println("\n");
+    System.out.println(" Today's Menu Is...");  
+    System.out.println("\n");
+    System.out.println("***************");   
+    System.out.println("\n");
+    
+    System.out.println("$"+soupPrice+"****"+"one cup of soup");
+    System.out.println("$"+kabab+"****"+"one cup of soup");
+    System.out.println("$"+taco+"****"+"one cup of soup");
+    
+              // $ 500.00 *** Slice of Big Rico Pizza
+             //$ 2.00 ***** Invisible Strawberry Pie
+             //$ 1.50 ***** Denver Omelet
+    
+  
+    
+    
+    }
+}
