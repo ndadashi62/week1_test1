3551f47d93af0e112652b96fbd8a8e31e063d265
ndadashi62 <nasim.dadashi@gmail.com>
2021-08-10 14:04:54 -0400

    AlltheMath has been added

--- exercise/src/main/java/com/mthree/exercise/ProjectGutenberg.java
+++ exercise/src/main/java/com/mthree/exercise/ProjectGutenberg.java
@@ -7,8 +7,17 @@ package com.mthree.exercise;
 
 /**
  *
- * @author mostafa
+ * @author nasim
  */
 public class ProjectGutenberg {
     
+     public static void main(String[] args) {
+        System.out.print("Did you know that in 1440 (or thereabouts),");
+        System.out.print("Johannes Gutenberg invented the printing press?");
+        System.out.print("He started out as a goldsmith!");
+        System.out.print("His invention made it easy to print and");
+        System.out.print("distribute books to anyone who wanted one.");
+        System.out.print("We are like a modern Gutenberg,");
+        System.out.print("printing vast amounts to the waiting console with ease.");
+    }
 }
