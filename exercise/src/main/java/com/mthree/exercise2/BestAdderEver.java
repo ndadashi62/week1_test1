71300793e0ff89be2ce31a040827d9a283013979
ndadashi62 <nasim.dadashi@gmail.com>
2021-08-10 16:12:50 -0400

    BestAdderEver added to the file and updated with the right codes

---
+++ exercise/src/main/java/com/mthree/exercise2/BestAdderEver.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mthree.exercise2;
+
+/**
+ *
+ * @author mostafa
+ */
+public class BestAdderEver {
+        public static void main(String[] args) {
+            
+            int a =  2;
+            int b = 5 ;
+            int c = 8 ; 
+            int d = a+b+c ; 
+            System.out.print (d);
+            
+            
+            
+        }
+    
+}
