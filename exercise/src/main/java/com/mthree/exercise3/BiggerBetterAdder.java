64f1b73dffebc68871b25483040f601969fadd39
ndadashi62 <nasim.dadashi@gmail.com>
2021-08-11 08:51:21 -0400

    BiggerBetterAdder added to the file

---
+++ exercise/src/main/java/com/mthree/exercise3/BiggerBetterAdder.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mthree.exercise3;
+
+/**
+ *
+ * @author nasim
+ */
+public class BiggerBetterAdder {
+    public static void main (String[] arg){
+    
+    int numOne= 5 ;
+    int numTwo=10 ; 
+    int numThree=15;
+    int sum=numOne+numTwo+numThree ; 
+        System.out.println(numOne + " + "+ numTwo + " + "+ numThree +" = "+ sum );
+
+    
+    
+    
+    
+    }
+    
+}
