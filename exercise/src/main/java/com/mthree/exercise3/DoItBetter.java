101e246c14c3e8896173f39d2bbca2b00aafde1c
ndadashi62 <nasim.dadashi@gmail.com>
2021-08-11 07:33:38 -0400

    DOitBetter added to the file

---
+++ exercise/src/main/java/com/mthree/exercise3/DoItBetter.java
@@ -0,0 +1,54 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mthree.exercise3;
+import java.util.Scanner;
+
+/**
+ *
+ * @author nasim
+ */
+public class DoItBetter {
+    public static void main(String[] args) {
+ 
+    
+    String name ;
+    int milesRun=0;
+    int hotDogNumber=0; 
+    int languageNumber =0;
+    int myNumbermile= milesRun*2 +1 ;
+    int myhotdogNumber= hotDogNumber*2+1;
+    int mylanguageNumber= languageNumber*2+1;
+    
+    
+      
+ Scanner inputReader = new Scanner(System.in);
+
+    System.out.println("Hello there!");
+    System.out.println("what is your name");
+  name= inputReader.nextLine();
+    System.out.println("hello "+ name +" .my name is Nasim");
+     
+ System.out.println("how many miles"+milesRun+" can you run  ");
+  Double.parseDouble(inputReader.nextLine());
+
+  System.out.println(" wow ! but me I can run "+myNumbermile +" every day ");
+  
+  
+  
+ System.out.println(" how many hot dog "+hotDogNumber+" can you eat every day");
+ Integer.parseInt(inputReader.nextLine());
+ System.out.println(" wow ! but me I can eat "+myhotdogNumber +"  hot dog every day ");
+ 
+ 
+
+ System.out.println(" how may "+languageNumber+"language you can speak");  
+ Integer.parseInt(inputReader.nextLine());
+  System.out.println(" wow ! but me I can talk "+mylanguageNumber+"  language number ");
+
+
+    }
+    
+}
