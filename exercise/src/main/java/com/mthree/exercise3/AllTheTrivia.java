c66a0f734db5064c37abe8fbf4217ba90957582f
ndadashi62 <nasim.dadashi@gmail.com>
2021-08-11 07:08:01 -0400

    AllTheTrivia is added to the file

---
+++ exercise/src/main/java/com/mthree/exercise3/AllTheTrivia.java
@@ -0,0 +1,44 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mthree.exercise3;
+import java.util.Scanner;
+
+/**
+ *
+ * @author nasim
+ */
+public class AllTheTrivia {
+    public static void main(String[] args) {
+    
+    
+    String unit ; 
+    String planet;
+    String volcano="l";
+    String element ="x";
+    Scanner scan= new Scanner (System.in);
+    
+    System.out.print("what unit is equiwalant to 1024");
+    unit=scan.nextLine();
+    
+        System.out.print("which planet is the only one that rotate clockwise");
+        planet=scan.nextLine();
+        
+        System.out.print("wow , 1024 gigabyte is a "+volcano+"!");
+        System.out.print("I didnt know that");
+        System.out.print("that's wonderful");
+        System.out.print(element+"is the only planet that rotates clockwise");
+        
+
+    
+    
+    
+    
+    
+    }
+    
+    
+    
+}
