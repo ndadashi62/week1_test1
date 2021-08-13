0a90adf398829fb844b2319712ef7c5b1b1eda37
ndadashi62 <nasim.dadashi@gmail.com>
2021-08-13 07:44:44 -0400

    LlamasWhalesAndDodosOhMy has been added to the file

--- exercise/src/main/java/com/mthree/exercise3/MiniMadLibs.java
+++ exercise/src/main/java/com/mthree/exercise3/MiniMadLibs.java
@@ -8,19 +8,7 @@ package com.mthree.exercise3;
 /**
  *
  * @author nasim
- *///I need a noun: Chocolate
-//Now an adjective: spooky
-//Another noun: Dodo
-///And a number: 10101
-//Another adjective: red
-//A plural noun: kittens
-//Another one: balls
-//One more: lettuce
-//A verb (infinitive form): sneeze
-//Same verb (past participle): sneezed
-
-//*** NOW LETS GET MAD (libs) ***
-//Chocolate: the spooky frontier. These are the voyages of the starship Dodo. Its 10101-year mission: to explore strange red kittens, to seek out red balls and red lettuce, to boldly sneeze where no one has sneezed before.
+ */
 
 public class MiniMadLibs {
     public static void main (String[] arg ){
