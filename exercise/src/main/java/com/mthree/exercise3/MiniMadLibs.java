a1e1994f8b30cb4bcf46352a99d383965595457a
ndadashi62 <nasim.dadashi@gmail.com>
2021-08-13 07:33:26 -0400

    MiniMadLibs added to the file

---
+++ exercise/src/main/java/com/mthree/exercise3/MiniMadLibs.java
@@ -0,0 +1,52 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mthree.exercise3;
+
+/**
+ *
+ * @author nasim
+ *///I need a noun: Chocolate
+//Now an adjective: spooky
+//Another noun: Dodo
+///And a number: 10101
+//Another adjective: red
+//A plural noun: kittens
+//Another one: balls
+//One more: lettuce
+//A verb (infinitive form): sneeze
+//Same verb (past participle): sneezed
+
+//*** NOW LETS GET MAD (libs) ***
+//Chocolate: the spooky frontier. These are the voyages of the starship Dodo. Its 10101-year mission: to explore strange red kittens, to seek out red balls and red lettuce, to boldly sneeze where no one has sneezed before.
+
+public class MiniMadLibs {
+    public static void main (String[] arg ){
+    
+        System.out.println("Let's play MAD LIBS!!");
+        
+        
+        
+        String noun ="chocolate";
+        String adjective="spooky";
+        String anothernoun ;
+        String pluralNoun="Kittens";
+        int number =878;
+        String PluralNoun;
+        
+        System.out.println("I need a noun:"+noun);
+        System.out.println("Now an adjective:"+adjective);
+        System.out.println("And a number"+noun);
+        System.out.println("Another adjective:"+adjective);
+        System.out.println("A plural noun:"+pluralNoun);
+        System.out.println("Another one"+pluralNoun);
+        
+        
+        
+
+    }
+    
+    
+}
