b502188c54ff8ec86b30fcb764bd6da257e16681
ndadashi62 <nasim.dadashi@gmail.com>
2021-08-14 08:07:56 -0400

    Healthy heart codes has been added to the file

--- exercise/src/main/java/com/mthree/AssessmentWeek1/HealthyHearts.java
+++ exercise/src/main/java/com/mthree/AssessmentWeek1/HealthyHearts.java
@@ -5,15 +5,26 @@
  */
 package com.mthree.AssessmentWeek1;
 
+import java.util.Scanner;
+
 /**
  *
  * @author nasim
  */
 public class HealthyHearts {
-    
-      public static void main(String[] args) {
-    
-    
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        //1: first we  ask the user for their dog name
+        System.out.println("Hello there !");
+        System.out.println("What is your age ?");
+
+        int userInput = Integer.parseInt(scanner.nextLine());
+        
+        
+        
+
     }
-    
+
 }
