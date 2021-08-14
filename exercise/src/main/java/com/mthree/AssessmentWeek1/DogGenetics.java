1d8175cb51d12fc19294f92eac657063844114bc
ndadashi62 <nasim.dadashi@gmail.com>
2021-08-14 07:55:58 -0400

    RockPaperscissor codes has been added to the file

--- exercise/src/main/java/com/mthree/AssessmentWeek1/DogGenetics.java
+++ exercise/src/main/java/com/mthree/AssessmentWeek1/DogGenetics.java
@@ -1,20 +1,65 @@
 package com.mthree.AssessmentWeek1;
 
+import java.util.Scanner;
+import java.util.Random;
+
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-
 /**
  *
  * @author nasim
  */
 public class DogGenetics {
-    
+
     public static void main(String[] args) {
-    
-    
+
+        Scanner scanner = new Scanner(System.in);
+
+        //1: first we  ask the user for their dog name
+        System.out.println("What is your dog's name?");
+        String userInput = scanner.nextLine();
+
+        //
+        System.out.println("Well then, I have this highly reliable report on  " + userInput + " Esquire's prestigious background right here./n");
+
+        // Sir Fluffy McFlufferkins Esquire is:
+        System.out.println(userInput + " Esquire is:");
+
+        //It should assign a random percentage to 5 dog breeds (that should add up to 100%!)
+        int percentage = 100;
+        Random randomizer = new Random();
+        int randomDogType1 = randomizer.nextInt(percentage);
+        percentage = percentage - randomDogType1;
+               
+        System.out.println(randomDogType1+"% GermsnShepered");
+        
+        
+
+        int randomDogType2 = randomizer.nextInt(percentage);
+        percentage = percentage - randomDogType2;
+        System.out.println(randomDogType2+" % Boxer");
+
+        int randomDogType3 = randomizer.nextInt(percentage);
+          percentage = percentage - randomDogType3;
+          System.out.println(randomDogType3+" % DobberMan");
+        
+        int randomDogType4 = randomizer.nextInt(percentage);
+        percentage = percentage - randomDogType4;
+        System.out.println(randomDogType4+" % Poodle");
+        
+        int randomDogType5 = randomizer.nextInt(percentage);
+        System.out.println(randomDogType5+" % Maltipo");
+        
+
+        
+
+      
+
+        System.out.println("Wow, that's QUITE the dog!");
+
     }
-    
+
 }
--- exercise/src/main/java/com/mthree/AssessmentWeek1/RockPaperScissors.java
+++ exercise/src/main/java/com/mthree/AssessmentWeek1/RockPaperScissors.java
@@ -7,114 +7,72 @@ package com.mthree.AssessmentWeek1;
 
 import java.util.Scanner;
 import java.util.Random;
+
 /**
  *
  * @author nasim
  */
 public class RockPaperScissors {
-    
-    public static void main (String[] args){
-        
-        
-        
-       // 1: // get the user input  
-    Scanner scanner =new Scanner (System.in);
-    Random randomizer = new Random();
-    System.out.println("Hello , welcome to this game, let's play\nEnter your move\ntype in rock, paper or scissors ");
-    String myMove= scanner.nextLine();
-    
-    
-    
-    
-    //2://verify that my move is valid 
-    if (myMove!="rock" && myMove!="paper" && myMove!="scissors"){
-    System.out.println ("your move is not valid !");
-    }
-   
-    else{
-     //3// randomly generate the opponents move(0,1,2)
-    for (int i=0; i<= 3; i++) {
-    int randomNumber = randomizer.nextInt(3);
-    System.out.println(randomNumber);
-    String opponentMove= "";
-    
-    if ( randomNumber==0){
-        opponentMove="rock"; 
-    }
-        
-   else if ( randomNumber==1){
-        opponentMove="paper"; 
-    }
-        
-    else{
-        opponentMove="scissor"; 
-    }
-    
-    
-    System.out.println("opnent move"+opponentMove);
-    
-   //4: //calculate if the user won , lost or tied 
-    if (myMove==opponentMove){
-    System.out.println("you tied !");
-    }
-    
-    else if (myMove=="rock" && opponentMove=="scissors"   
-            ||myMove== "scissors" && opponentMove=="paper"
-            ||myMove== "paper" && opponentMove=="rock"
-            )
-    { System.out.println("you won !");
-    }
-            
-            else {
-                    System.out.println("you lost !") ;
+
+    public static void main(String[] args) {
+
+        int tie = 0;
+        int userWin = 0;
+        int opponentWin = 0;
+        // 1: // get the user input  
+        Scanner scanner = new Scanner(System.in);
+        Random randomizer = new Random();
+        System.out.println("Hello , welcome to this game, let's play\n");
+        String playAgain = "n";
+        do {
+            System.out.println("how many round do you want to play?");
+            int round = Integer.parseInt(scanner.nextLine());
+            for (int i = 0; i < round; i++) {
+                System.out.println("your choice:");
+                String myMove = scanner.nextLine();
+                //2://verify that my move is valid
+                if (myMove != "rock" && myMove != "paper" && myMove != "scissors") {
+//                System.out.println("your move is not valid !");
+//            } else {
+                    //3// randomly generate the opponents move(0,1,2)
+                    int randomNumber = randomizer.nextInt(3);
+                    System.out.println(randomNumber);
+                    String opponentMove = "";
+                    if (randomNumber == 0) {
+                        opponentMove = "rock";
+                    } else if (randomNumber == 1) {
+                        opponentMove = "paper";
+                    } else {
+                        opponentMove = "scissor";
                     }
-               
+                    System.out.println("opnent move:" + opponentMove);
+
+                    //4: //calculate if the user won , lost or tied 
+                    if (myMove == opponentMove) {
+                        tie++;
+                    } else if (myMove == "rock" && opponentMove == "scissors"
+                            || myMove == "scissors" && opponentMove == "paper"
+                            || myMove == "paper" && opponentMove == "rock") {
+                        userWin++;
+                    } else {
+                        opponentWin++;
+                    }
+
+                }
+            }
+            if (userWin > opponentWin) {
+                System.out.println("you Won");
+            } else if (opponentWin > userWin) {
+                System.out.println("you Lost");
+            } else {
+                System.out.println("Tie");
+            }
             
+            System.out.println("play again(y/n):");
+            playAgain = scanner.nextLine();
             
-            
-            
-           
-        
-   
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
+        } while (playAgain == "y");
         
     }
-}
-    
-    }
-   
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-}
-  
-    
-    
-    
-    
-    
+
 }
